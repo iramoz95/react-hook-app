8c7fe4c7e1f70bc1928763cd0641ae763305a5e3
iramoz95 <ianramoz95@gmail.com>
2022-04-21 16:04:20 -0500

    useLayoutEffect examples

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ import ReactDOM from "react-dom/client";
 //import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
 //import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
 //import { FocusScreen } from "./components/04-useRef/FocusScreen";
-import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
+//import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
+import { Layout } from "./components/05-useLayoutEffect/Layout";
 
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RealExampleRef />);
+root.render(<Layout />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
